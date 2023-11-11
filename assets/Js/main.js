f5b17fd2f32dfdc487fcea9ec0b8e5d60a444cf7
sarybuitrago17 <130333781+sarybuitrago17@users.noreply.github.com>
2023-11-11 14:55:27 -0500

    Adopta-un-perro

---
+++ assets/Js/main.js
@@ -0,0 +1,206 @@
+
+const Registros = []
+  console.log(Registros)
+
+
+  function Registro() {
+    const carta1 = {
+      WhatsApp: "",
+        img: "",
+         Nombre: "",
+         Edad: "",
+          Raza: "",
+         Descripcion: "",
+         Vacunas: {
+           Nombre: "",
+           Para: ""
+         }
+       }
+       const No = document.getElementById("No").value
+       carta1.WhatsApp = No
+    const image = document.getElementById("img").value
+    carta1.img = image
+    const name = document.getElementById("name").value
+    carta1.Nombre = name
+    const edad = document.getElementById("edad").value
+    carta1.Edad = edad
+    const raza = document.getElementById("Raza").value
+    carta1.Raza = raza
+    const des = document.getElementById("des").value
+    carta1.Descripcion = des
+    const vacuna = document.getElementById("nombreVacuna").value
+    carta1.Vacunas.Nombre = vacuna
+    const para = document.getElementById("para").value
+    carta1.Vacunas.Para = para
+
+
+    if (Object.values(carta1).some(value => value !== "")) {
+        Registros.push(carta1);
+      }
+      const op = document.getElementById("Select_Razas");
+    op.innerHTML = ""; 
+      const RazasUnicas = new Set(Registros.map((R) => R.Raza));
+
+    RazasUnicas.forEach((raza) => {
+        const option = document.createElement("option");
+        option.value = raza;
+        option.text = raza;
+        op.add(option);
+    });
+
+    console.log(RazasUnicas);
+
+    document.getElementById("Select_Razas").addEventListener("change", function () {
+        const selectedRaza = this.value;
+        const filteredRegistros = Registros.filter((registro) => registro.Raza === selectedRaza);
+    
+        const NameDog = document.getElementById("Dog");
+        NameDog.innerHTML = "";
+    
+        filteredRegistros.forEach((perro) => {
+            const est = `
+            <div class="container mt-4">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1 class="display-4">¡Hola, soy ${perro.Nombre}!</h1>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h1>Edad: <span>${perro.Edad}</span></h1>
+                        </div>
+                        <div class="col-md-6">
+                            <h1>Raza: <span>${perro.Raza}</span></h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h1>Sexo: <span>Hembra</span></h1>
+                        </div>
+                        <div class="col-md-6">
+                            <h1>Esterilizada: <span>No</span></h1>
+                        </div>
+                    </div>
+                    <p class="mt-3">${perro.Descripcion}</p>
+                </div>
+                <div class="col-md-6">
+                    <img class="img-fluid" src="${perro.img}" alt="">
+                </div>
+            </div>
+        </div>
+            `;
+    
+            NameDog.innerHTML += est;
+        });
+    });
+  const NameDog = document.getElementById("Dog")
+  NameDog.innerHTML = "";
+
+  Registros.forEach(perro => {
+    const est = `
+    <div class="container mt-4">
+    <div class="row">
+        <div class="col-md-6">
+            <h1 class="display-4">¡Hola, soy ${perro.Nombre}!</h1>
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>Edad: <span>${perro.Edad}</span></h1>
+                </div>
+                <div class="col-md-6">
+                    <h1>Raza: <span>${perro.Raza}</span></h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>Sexo: <span>Hembra</span></h1>
+                </div>
+                <div class="col-md-6">
+                    <h1>Esterilizada: <span>No</span></h1>
+                </div>
+            </div>
+            <p class="mt-3">${perro.Descripcion}</p>
+        </div>
+        <div class="col-md-6">
+            <img class="img-fluid" src="${perro.img}" alt="">
+        </div>
+    </div>
+</div>
+
+    `;
+
+
+    NameDog.innerHTML  += est
+
+  })
+  }
+
+  document.getElementById("open").addEventListener("click", function() {
+    document.querySelector(".form").style.display = 'grid';
+    console.log("object")
+  })
+
+  document.getElementById("CloseForm").addEventListener("click", function() {
+    document.querySelector(".form").style.display = 'none';
+  })
+  document.getElementById("MostrarTodos").addEventListener("click", function () {
+    const NameDog = document.getElementById("Dog");
+    NameDog.innerHTML = "";
+
+    Registros.forEach((perro) => {
+        const est = `
+        <div class="con container  mt-4">
+        <div class="row">
+            <div class="col-md-6">
+                <h2 class="display-4">¡Hola, soy ${perro.Nombre}!</h2>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h1>Edad: <span>${perro.Edad}</span></h1>
+                    </div>
+                    <div class="col-md-6">
+                        <h1>Raza: <span>${perro.Raza}</span></h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h1>Sexo: <span>Hembra</span></h1>
+                    </div>
+                    <div class="col-md-6">
+                        <h1>Esterilizada: <span>No</span></h1>
+                    </div>
+                </div>
+                <p class="mt-3">${perro.Descripcion}</p>
+            </div>
+            <div class="col-md-6">
+                <img class="img-fluid" src="${perro.img}" alt="">
+            </div>
+        </div>
+    </div>
+  
+        `;
+
+        NameDog.innerHTML += est;
+    });
+});
+
+
+function transitionNavbar() {
+    const navbar = document.querySelector(".navba");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 250) {
+            navbar.classList.add("navbar__active");
+        } else {
+            navbar.classList.remove("navbar__active");
+        }
+    });
+}
+
+function handleTheme() {
+    const iconTheme = document.querySelector(".bi-moon");
+
+    iconTheme.addEventListener("click", function () {
+        document.body.classList.toggle("dark-theme");
+    });
+}
+
+transitionNavbar();
+handleTheme();
+
